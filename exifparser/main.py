cc015252e340d1ca644261a8cb1c4c9102c7cf91
joopeti <pyry.joona@helsinki.fi>
2015-05-22 15:00:10 +0300

    exifparser analysoi kansion kaikki kuvat ja antaa niistä valotusajan perusteella pisteytyksen

--- exifparser/main.py
+++ exifparser/main.py
@@ -5,8 +5,7 @@ import os
 import exifread
 import sys
 import math
-
-folder = os.getcwd()
+import glob
 
 # iso =  tags['EXIF ISOSpeedRatings']
 # length35mm = float(tags['EXIF FocalLengthIn35mmFilm'].printable)
@@ -14,20 +13,16 @@ folder = os.getcwd()
 # To be done:
 # def calculateEffectiveFocalLength(focal_length):
 
-def getExposureRatio(tags):
-	if "EXIF ExposureTime" in tags:
-		exposure = tags["EXIF ExposureTime"]
-		exposure = eval(exposure.printable)
-		if exposure < 1/1000: # alle 1/1000 valotusaika lasketaan teräväksi
-			return 0.0;
-		if exposure > 1/4: # yli 1/4 valotusaika lasketaan epäteräväksi
-			return 1.0
-		exposure = math.log(exposure, 2)
-		min = -10 
-		max = -2 
-		normalized = (exposure-(min))/(max-min)
-		return normalized
-	return "Couldn't find exposure-data"
+def getExposureRatio(exposure):
+	if exposure < 1/1000: # alle 1/1000 valotusaika lasketaan teräväksi
+		return 0.0;
+	if exposure > 1/4: # yli 1/4 valotusaika lasketaan epäteräväksi
+		return 1.0
+	exposure = math.log(exposure, 2)
+	min = -10 
+	max = -2 
+	normalized = (exposure-(min))/(max-min)
+	return normalized
 
 def parseExif(pathToImage):
 
@@ -40,7 +35,22 @@ def parseExif(pathToImage):
 	return tags;
 
 if __name__ == "__main__":
-	tiedosto = sys.argv[1]
-	tags = parseExif(tiedosto)
-	print getExposureRatio(tags)
+	# tiedosto = sys.argv[1]
+	# folder = os.getcwd()
+	points = 0
+	num_files = 0
+	
+	for tiedosto in glob.glob('*.jpg'):
+		tags = parseExif(tiedosto)
+		if "EXIF ExposureTime" in tags:
+			exposure = tags["EXIF ExposureTime"]
+			exposure = eval(exposure.printable)
+			# print tiedosto + ": "
+			ratio = getExposureRatio(exposure)
+			print ratio
+			points += ratio
+			num_files += 1
+			
+	print "Keskiarvo: "
+	print points / num_files
 

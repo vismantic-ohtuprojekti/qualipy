fa2983aa13fee16db5202419b07afb9589431785
ejaasaari <elias.jaasaari@gmail.com>
2015-05-25 20:12:32 +0300

    Add tests

---
+++ tests/test_utils.py
@@ -0,0 +1,8 @@
+import numpy
+
+from imgfilter.utils import *
+
+
+def test_partition_matrix_returns_correct_number_of_partitions():
+    mat = numpy.random.randn(100).reshape((10, 10))
+    assert len(partition_matrix(mat, 3)) == 10

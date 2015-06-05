47824225822d0d62b7d5a943845742a4ea2d4d46
joopeti <pyry.joona@helsinki.fi>
2015-06-05 18:04:26 +0300

    commented broken test

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -2,7 +2,7 @@ import numpy
 
 from imgfilter.utils import *
 
-""" Tän vois korjaa joskus
+""" Needs to be fixed
 def test_partition_matrix_returns_correct_number_of_partitions():
     mat = numpy.random.randn(100).reshape((10, 10))
     assert len(partition_matrix(mat, 3)) == 10

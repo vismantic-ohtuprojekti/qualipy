cab8530f8bdfec484a638e65198e6f44e68e1dc1
Henri Korpela <Hicks48@1BQQGK1.kumpula.dhcp.hupnet.helsinki.fi>
2015-06-09 14:05:19 +0300

    Pattern detection

---
+++ imgfilter/algorithms/common/statistic_common.py
@@ -0,0 +1,109 @@
+import numpy as np
+
+
+def avarage(array_1D):
+    return np.sum(array_1D) / array_1D.shape[0]
+
+def get_max_values(array_1D, count):
+    max_values = np.array([])
+    sorted_array = np.sort(array_1D)
+    for i in range(sorted_array.shape[0] - 1, sorted_array.shape[0] - count - 1, -1):
+        if i < 0 or i >= sorted_array.shape[0]:
+            break
+        max_values = np.append(max_values, sorted_array[i])
+    return max_values
+
+
+def linear_normalize(value, min_value, max_value):
+    return (value - min_value) / max_value
+
+
+def linear_normalize_all(array_1D):
+    max_value = np.amax(array_1D)
+    min_value = np.amin(array_1D)
+
+    scaled = np.array([])
+
+    for entry in np.nditer(array_1D):
+        scaled = np.append(scaled, linear_normalize(entry, min_value, max_value))
+
+    return scaled
+
+
+def count_local_outliner_factor(entry, neighbors):
+    outliners = np.array([])
+
+    for neighbor in neighbors:
+        outliners = np.append(outliners, np.absolute(entry - neighbor))
+
+    return np.sum(outliners) / outliners.shape[0]
+
+def find_neighbors2(index, k, sorted_array):
+    neighbors = np.array([])
+    left_index = index - 1
+    right_index = index + 1
+
+    while neighbors.shape[0] < k:
+        dist_left = float('inf')
+        dist_right = float('inf')
+
+        if left_index >= 0:
+            dist_left = np.absolute(sorted_array[index] - sorted_array[left_index])
+
+        if right_index < sorted_array.shape[0]:
+            dist_right = np.absolute(sorted_array[index] - sorted_array[right_index])
+
+        if left_index < 0 and right_index >= sorted_array.shape[0]:
+            break
+
+        if dist_left <= dist_right:
+            neighbors = np.append(neighbors, sorted_array[left_index])
+            left_index = left_index - 1
+        else:
+            neighbors = np.append(neighbors, sorted_array[right_index])
+            right_index = right_index + 1
+
+    return neighbors
+
+
+def find_neighbors(index, k, sorted_array):
+    neighbors = np.array([])
+
+    neighbor_index = index - (k/2)
+    while neighbors.shape[0] < k:
+        if neighbor_index >= sorted_array.shape[0]:
+            break
+
+        if neighbor_index == index:
+            neighbor_index = neighbor_index + 1
+            continue
+
+        if neighbor_index >= 0:
+            neighbors = np.append(neighbors, sorted_array[neighbor_index])
+
+        neighbor_index = neighbor_index + 1
+
+    return neighbors
+
+
+def remove_anomalies(array_1D, max_outline_diff):
+    sorted_array = np.sort(array_1D)
+    outliner_factors = np.array([])
+    k = 4
+
+    # Count outline factors
+    for i in range(0, sorted_array.shape[0]):
+        neighbors = find_neighbors2(i, k, sorted_array)
+        outliner_factor = count_local_outliner_factor(sorted_array[i], neighbors)
+        outliner_factors = np.append(outliner_factors, outliner_factor)
+
+
+    anomalies_removed = np.array([])
+    outliner_factors = linear_normalize_all(outliner_factors)
+    outliner_factor_avg = np.sum(outliner_factors) / outliner_factors.shape[0]
+
+    for i in range(0, outliner_factors.shape[0]):
+        if np.absolute(outliner_factors[i] - outliner_factor_avg) <= max_outline_diff or outliner_factors[i] <= outliner_factor_avg:
+            anomalies_removed = np.append(anomalies_removed, sorted_array[i])
+
+    return anomalies_removed
--- imgfilter/filters/pattern_detection.py
+++ imgfilter/filters/pattern_detection.py
@@ -1,34 +1,11 @@
+import statistic_common
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-def get_max_values(array_1D, count):
-    max_values = np.array([])
-    sorted = np.sort(array_1D)
-    for i in range(array_1D.shape[0] - 1, array_1D.shape[0] - count - 1, -1):
-        max_values = np.append(max_values, array_1D[i])
-    return max_values
 
-
-def avarage(array_1D):
-    return np.sum(array_1D) / array_1D.shape[0]
-
-
-def scale(value, min_value, max_value):
-    return (value - min_value) / max_value
-
-def remove_anomalies(array_1D, avarage, max_anonamaly, min_value, max_value):
-    valid_values = np.array([])
-    scaled_avarage = scale(avarage, min_value, max_value)
-
-    for i in range(0, array_1D.shape[0]):
-        value = scale(array_1D[i], min_value, max_value)
-        if value < scaled_avarage + max_anonamaly and value > scaled_avarage - max_anonamaly:
-            valid_values = np.append(valid_values, array_1D[i])
-
-    return valid_values
-
-def all_points_in_circle(array_2D, radii):
+def all_points_in_circle(array_2D, radii, image):
     all_points = 0.0
     center = np.array((array_2D.shape[0]/2.0, array_2D.shape[1]/2.0))
 
@@ -38,7 +15,14 @@ def all_points_in_circle(array_2D, radii):
 
             if distance_from_center <= radii:
                 all_points = all_points + 1.0
+            else:
+                array_2D[x,y] = 0.5
 
+    plt.figure('Test')
+    plt.subplot(121),plt.imshow(array_2D, cmap = 'gray')
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(image, cmap = 'gray')
+    plt.show()
     return all_points
 
 
@@ -72,19 +56,18 @@ def count_magnitude_spectrum(image):
 
 
 def pattern_regonition(image_path):
-    image = cv2.imread(image_path)
-    image = reduce_colors(image, 2)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Reduce colors and turn into gray scale
+    image = cv2.imread(image_path, 0)
+    #image = reduce_colors(image, 2)
+    #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    # Count magnitude spectrum
     magnitude_spectrum = count_magnitude_spectrum(image)
 
+    # Center of the image
     center = np.array((magnitude_spectrum.shape[0]/2.0, magnitude_spectrum.shape[1]/2.0))
-    radii = (1.0/6.0) * np.linalg.norm(center)
 
-    max_distance = 0.0
     all_distances = np.array([])
-    dist_sum = 0.0
-
     non_zero_points = 0.0
 
     for x in range(0, magnitude_spectrum.shape[0]):
@@ -94,26 +77,23 @@ def pattern_regonition(image_path):
 
             # Make magnitude spectrum to contain only high values
             # and count all non zero points
-            if magnitude_spectrum[x,y] > 0.70:
+            if magnitude_spectrum[x,y] > 0.80:
                 magnitude_spectrum[x,y] = 1.0
-                dist_sum = dist_sum + distance_from_center
                 all_distances = np.append(all_distances, distance_from_center)
                 non_zero_points = non_zero_points + 1
             else:
                 magnitude_spectrum[x,y] = 0.0
 
-            # Update max distance
-            if distance_from_center > max_distance:
-                max_distance = distance_from_center
 
-    #print "max distance: ", max_distance, " for image: ", image_path
-    avg_dist = dist_sum / non_zero_points
-    all_distances = remove_anomalies(all_distances, avg_dist, 0.3, 0.0, max_distance)
-    max_distance = avarage(get_max_values(all_distances, 20))
-    all_points = all_points_in_circle(magnitude_spectrum, max_distance)
+    all_distances = statistic_common.remove_anomalies(all_distances, 0.2)
+    max_distances = statistic_common.get_max_values(all_distances, 20)
+
+    max_distance_avg = statistic_common.avarage(max_distances)
+    all_points = all_points_in_circle(magnitude_spectrum, max_distance_avg, image)
     return non_zero_points / all_points
 
 
+print 'patterns/Shadowgame.jpg: ', pattern_regonition('patterns/Shadowgame.jpg')
 print "Pattern images:"
 print 'patterns/pattern_0.jpg: ', pattern_regonition('patterns/pattern_0.jpg')
 print 'patterns/pattern_1.png: ', pattern_regonition('patterns/pattern_1.png')
@@ -127,19 +107,19 @@ print 'patterns/pattern_9.png: ',pattern_regonition('patterns/pattern_9.jpg')
 print 'patterns/pattern_10.png: ',pattern_regonition('patterns/pattern_10.jpg')
 
 print "Non pattern images:"
-print 'patterns/non_pattern_1.jpg: ', pattern_regonition('patterns/non_pattern_1.jpg')
-print 'patterns/non_pattern_2.jpg: ', pattern_regonition('patterns/non_pattern_2.jpg')
-print 'patterns/non_pattern_3.jpg: ', pattern_regonition('patterns/non_pattern_3.jpg')
-print 'patterns/non_pattern_4.jpg: ', pattern_regonition('patterns/non_pattern_4.jpg')
-print 'patterns/non_pattern_5.jpg: ', pattern_regonition('patterns/non_pattern_5.jpg')
-print 'patterns/non_pattern_6.jpg: ', pattern_regonition('patterns/non_pattern_6.jpg')
-print 'patterns/non_pattern_7.jpg: ', pattern_regonition('patterns/non_pattern_7.jpg')
-print 'patterns/non_pattern_8.jpg: ', pattern_regonition('patterns/non_pattern_8.jpg')
-print 'patterns/non_pattern_9.jpg: ', pattern_regonition('patterns/non_pattern_9.jpg')
-print 'patterns/non_pattern_10.jpg: ', pattern_regonition('patterns/non_pattern_10.jpg')
+print 'non_pattern/non_pattern_1.jpg: ', pattern_regonition('non_pattern/non_pattern_1.jpg')
+print 'non_pattern/non_pattern_2.jpg: ', pattern_regonition('non_pattern/non_pattern_2.jpg')
+print 'non_pattern/non_pattern_3.jpg: ', pattern_regonition('non_pattern/non_pattern_3.jpg')
+print 'non_pattern/non_pattern_4.jpg: ', pattern_regonition('non_pattern/non_pattern_4.jpg')
+print 'non_pattern/non_pattern_5.jpg: ', pattern_regonition('non_pattern/non_pattern_5.jpg')
+print 'non_pattern/non_pattern_6.jpg: ', pattern_regonition('non_pattern/non_pattern_6.jpg')
+print 'non_pattern/non_pattern_7.jpg: ', pattern_regonition('non_pattern/non_pattern_7.jpg')
+print 'non_pattern/non_pattern_8.jpg: ', pattern_regonition('non_pattern/non_pattern_8.jpg')
+print 'non_pattern/non_pattern_9.jpg: ', pattern_regonition('non_pattern/non_pattern_9.jpg')
+print 'non_pattern/non_pattern_10.jpg: ', pattern_regonition('non_pattern/non_pattern_10.jpg')
 
 print "Random pattern like"
-print 'patterns/random_1.jpg: ', pattern_regonition('patterns/random_1.jpg')
-print 'patterns/random_2.jpg: ', pattern_regonition('patterns/random_2.jpg')
-print 'patterns/random_3.jpg: ', pattern_regonition('patterns/random_3.jpg')
-print 'patterns/random_4.jpg: ', pattern_regonition('patterns/random_4.jpg')
+print 'random/random_1.jpg: ', pattern_regonition('random/random_1.jpg')
+print 'random/random_2.jpg: ', pattern_regonition('random/random_2.jpg')
+print 'random/random_3.jpg: ', pattern_regonition('random/random_3.jpg')
+print 'random/random_4.jpg: ', pattern_regonition('random/random_4.jpg')

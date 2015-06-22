ff2e1c97f4c4b58321b00dd2a8cbf17069bd239e
joopeti <pyry.joona@helsinki.fi>
2015-06-22 22:41:05 +0300

    bugfix

--- imgfilter/filters/over_exposure.py
+++ imgfilter/filters/over_exposure.py
@@ -20,6 +20,7 @@ class OverExposed(Filter):
         """
         image = self.parameters['image']
         histogram = cv2.calcHist([image], [0], None, [256], [0, 256])
+        # Normalize:
         clip = clipping_percentage(histogram, 250) * 50
         return min(1, clip)
 
@@ -28,5 +29,4 @@ def clipping_percentage(histogram, threshold):
     total = numpy.sum(histogram)
     if total < 0.0005:  # avoid division by zero
         return 0
-
     return float(numpy.sum(histogram[threshold:])) / total

9fd72888166bb708736aad294ca03afe710dc707
morbeus <velipekka.s.hamalainen@gmail.com>
2015-06-24 23:15:33 +0300

    Added test for under exposure

--- tests/filters/test_under_exposure.py
+++ tests/filters/test_under_exposure.py
@@ -2,18 +2,17 @@ import cv2
 
 from imgfilter.utils.utils import clipping_percentage
 
-
-IMAGE = cv2.imread('tests/images/lama.jpg', 0)
+IMAGE = cv2.imread('tests/images/uni_scene1_minus_3.png', 0)
 HIST = cv2.calcHist([IMAGE], [0], None, [256], [0, 256])
 
 
 def test_clipping_percentage_returns_one_if_threshold_is_zero():
-    assert round(clipping_percentage(HIST, 0, True), 5) == 1
+    assert round(clipping_percentage(HIST, 0, False), 5) == 0
 
 
 def test_clipping_percentage_returns_zero_if_threshold_is_max():
-    assert round(clipping_percentage(HIST, 256, True), 5) == 0
+    assert round(clipping_percentage(HIST, 256, False), 5) == 1
 
 
 def test_clipping_percentage_returns_correct_value():
-    assert round(clipping_percentage(HIST, 250, True), 3) == 0.004
+    assert round(clipping_percentage(HIST, 5, False), 3) == .023

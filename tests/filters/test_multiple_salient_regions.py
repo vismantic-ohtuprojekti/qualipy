da11d8862227c5df8860ee0929e4a8f44d8a417f
ejaasaari <elias.jaasaari@gmail.com>
2015-07-13 17:59:08 +0300

    Fix failing tests

--- tests/filters/test_multiple_salient_regions.py
+++ tests/filters/test_multiple_salient_regions.py
@@ -2,17 +2,23 @@ import numpy
 
 from imgfilter.filters.multiple_salient_regions import *
 
-def test_works_for_saliency_map_with_two_areas():
-    saliency_map = np.array([0, 0, 255, 255], [0, 0, 0, 0], [255, 255, 0, 0])
+
+def run_filter(saliency_map):
     filter = MultipleSalientRegions()
     filter.parameters['extract_object'] = (saliency_map, 0)
-
-    assert (filter.run() > 0.5)
+    return filter.run()
 
 
 def test_works_for_saliency_map_with_one_area():
-    saliency_map = np.array([0, 0, 0, 0], [0, 0, 0, 0], [255, 255, 0, 0])
-    filter = MultipleSalientRegions()
-    filter.parameters['extract_object'] = (saliency_map, 0)
+    saliency_map = numpy.zeros((100, 100), dtype=numpy.uint8)
+    saliency_map[10:20, 10:20] = 255
 
-    assert (filter.run() < 0.5)
+    assert run_filter(saliency_map) < 0.5
+
+
+def test_works_for_saliency_map_with_two_areas():
+    saliency_map = numpy.zeros((100, 100), dtype=numpy.uint8)
+    saliency_map[10:20, 10:20] = 255
+    saliency_map[80:90, 80:90] = 255
+
+    assert run_filter(saliency_map) > 0.5

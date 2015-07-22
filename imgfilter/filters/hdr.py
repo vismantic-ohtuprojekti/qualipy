519b52fbe6ddde6190c4f32e0436c10a13be6318
ejaasaari <elias.jaasaari@gmail.com>
2015-07-22 17:24:51 +0300

    Add docstrings, code cleanup

--- imgfilter/filters/hdr.py
+++ imgfilter/filters/hdr.py
@@ -2,23 +2,22 @@ import cv2
 import numpy
 
 from imgfilter.machine_learning.svm import SVM
-from imgfilter.utils.utils import scaled_prediction, partition_matrix
+from imgfilter.utils.utils import *
 from .. import get_data
 
 from filter import Filter
 
 
-def edge_ratio(obj):
-    return numpy.count_nonzero(obj) / float(obj.size)
-
-
-def normalize(arr):
-    arr_min = numpy.min(arr)
-    arr_max = numpy.max(arr)
-    return (arr - arr_min) / float(arr_max - arr_min)
-
-
 def histogram_features(hist, maximum, step):
+    """Get features from a histogram.
+
+    :param hist: a histogram representing a color channel
+    :type hist: numpy.ndarray
+    :param maximum: maximum value of the color channel
+    :type maximum: int
+    :param step: in how big steps should the histogram be sampled
+    :type step: int
+    """
     means, maxes, max_diffs = [], [], []
 
     for i in xrange(step, maximum, step):
@@ -32,8 +31,17 @@ def histogram_features(hist, maximum, step):
         numpy.array(max_diffs, dtype=numpy.float32)
 
 
-def color_channel_feature(space, maximum, step):
-    hist = cv2.calcHist([space.flatten()], [0], None, [maximum],
+def color_channel_feature(channel, maximum, step):
+    """Calculate features from a color channel based on its histogram.
+
+    :param channel: the color channel
+    :type channel: numpy.ndarray
+    :param maximum: maximum value of the color channel
+    :type maximum: int
+    :param step: in how big steps should the histogram be sampled
+    :type step: int
+    """
+    hist = cv2.calcHist([channel.flatten()], [0], None, [maximum],
                         [0, maximum - 1]).T[0]
 
     features = tuple([normalize(feat) for feat in
@@ -41,19 +49,42 @@ def color_channel_feature(space, maximum, step):
     return numpy.concatenate(features)
 
 
+def RMS_contrast(luminance):
+    """Calculates the RMS contrast of an image.
+    See https://en.wikipedia.org/wiki/Contrast_%28vision%29#RMS_contrast
+
+    :param luminance: matrix representing luminance of each image pixel
+    :type luminance: numpy.ndarray
+    :returns: numpy.ndarray
+    """
+    norm = numpy.array(luminance, dtype=numpy.float32).flatten() / 255.
+    intensities = (numpy.sum(norm) / norm.size - norm) ** 2
+    return numpy.sum(intensities) / norm.size
+
+
 def contrast(luminance):
+    """Calculates the contrast in parts of an image.
 
-    def RMS_contrast(view):
-        norm = numpy.array(view, dtype=numpy.float32).flatten() / 255.
-        intensities = (numpy.sum(norm) / norm.size - norm) ** 2
-        return numpy.sum(intensities) / norm.size
-
+    :param luminance: matrix representing luminance of each image pixel
+    :type luminance: numpy.ndarray
+    :returns: numpy.ndarray
+    """
     parts = partition_matrix(luminance, 16)
     return numpy.array([RMS_contrast(part) for part in parts],
                        dtype=numpy.float32)
 
 
+def edge_ratio(obj):
+    return numpy.count_nonzero(obj) / float(obj.size)
+
+
 def edges(gray):
+    """Calculate ratio of edges present in parts of an image.
+
+    :param gray: grayscale image matrix
+    :type gray: numpy.ndarray
+    :returns: numpy.ndarray
+    """
     gray_lap = cv2.Laplacian(gray, cv2.CV_16S, ksize=3)
     edges = cv2.convertScaleAbs(gray_lap)
     parts = partition_matrix(edges, 16)
@@ -63,9 +94,16 @@ def edges(gray):
 
 
 def get_input_vector(image):
+    """Get input vector for use in SVM.
+
+    :param image: the color image matrix
+    :type img: numpy.ndarray
+    :returns: numpy.ndarray -- the input vector
+    """
     XYZ = cv2.cvtColor(image, cv2.COLOR_BGR2XYZ)
     YUV = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
     X, Y, Z = cv2.split(XYZ)
     y, u, v = cv2.split(YUV)
 
@@ -95,6 +133,10 @@ class HDR(Filter):
         return {'color_image'}
 
     def run(self):
+        """Detects if an image is a HDR image.
+
+        :returns: float
+        """
         svm = SVM()
         svm.load(get_data('svm/hdr.yml'))
 

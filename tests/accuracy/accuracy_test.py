6e6c0066998b90b483019373816e8e3bca3f2551
ejaasaari <elias.jaasaari@gmail.com>
2015-08-14 18:57:42 +0300

    Add scripts for accuracy tests

---
+++ tests/accuracy/accuracy_test.py
@@ -0,0 +1,26 @@
+import sys
+
+import imgfilter
+import accuracy_test_lib
+
+
+def run_accuracy_test(filter):
+    if len(sys.argv) != 3:
+        sys.stderr.write('Usage: %s POSITIVES NEGATIVES\n' % sys.argv[0])
+        sys.exit(1)
+
+    positives = [sys.argv[1]]
+    negatives = [sys.argv[2]]
+    extensions = ['.jpg', '.JPG', '.jpeg', '.png']
+
+    def get_prediction(path):
+        return not imgfilter.process(path, [filter])
+
+    test = accuracy_test_lib.AccuracyTest(get_prediction,
+            ((True, positives), (False, negatives)), extensions)
+    test.run()
+
+    result = test.get_result()
+    print 'Accuracy for all samples: ', result.get_accuracy()
+    print 'Accuracy for positive images: ', result.get_accuracy_for_type(True)
+    print 'Accuracy for negative images: ', result.get_accuracy_for_type(False)

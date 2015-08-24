5051b66cdc8d509c086b5addbf200c206612632b
morbeus <vhamalai@cs.helsinki.fi>
2015-08-24 13:22:29 +0300

    changed textfilter

--- imgfilter/filters/text_detection.py
+++ imgfilter/filters/text_detection.py
@@ -6,12 +6,13 @@ from filter import Filter
 class TextDetection(Filter):
 
     name = 'text_detection'
-    speed = 10
+    speed = 3
 
     def __init__(self, threshold=0.5, invert_threshold=False):
         super(TextDetection, self).__init__(threshold, invert_threshold)
 
     def predict(self, image_path, return_boolean=True, ROI=None):
+        
         if len(img_to_str('tesseract', image_path)) > 1:
             prediction = 1.0
         else:

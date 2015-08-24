005e7e4bda22a9cdf3d49899e2274d31d7f86f96
morbeus <vhamalai@cs.helsinki.fi>
2015-08-24 13:56:51 +0300

    added documentation to textfilter and to its utils.

--- imgfilter/filters/text_detection.py
+++ imgfilter/filters/text_detection.py
@@ -1,3 +1,12 @@
+"""
+Filter for detecting text in images(uses tesseract-ocr)
+
+Uses tesseract-ocr through a python wrapper, which creates a tesseract-ocr
+process and feeds the input image to it. Then filter makes prediction
+based on the string the tesseract-returns to it. If the string length is
+longer than one then there is considered to be a text in the image.
+"""
+
 from ..utils.pytesseract import img_to_str
 
 from filter import Filter
@@ -5,14 +14,37 @@ from filter import Filter
 
 class TextDetection(Filter):
 
+    """Filter for detecting text in images(uses tesseract-ocr"""
+
     name = 'text_detection'
     speed = 3
 
     def __init__(self, threshold=0.5, invert_threshold=False):
+        """Initializes a text detection filter
+
+        :param threshold: threshold at which the given prediction is changed
+                          from negative to positive
+        :type threshold: float
+        :param invert_threshold: whether the result should be greater than
+                                 the given threshold (default) or lower
+                                 for an image to be considered positive
+        :type invert_threshold: bool"""
         super(TextDetection, self).__init__(threshold, invert_threshold)
 
     def predict(self, image_path, return_boolean=True, ROI=None):
-        
+        """Predict if a given image has text.
+
+        :param image_path: path to the image
+        :type image_path: str
+        :param return_boolean: whether to return the result as a
+                               float between 0 and 1 or as a boolean
+                               (threshold is given to the class)
+        :type return_boolean: bool
+        :param ROI: possible region of interest as a 4-tuple
+                    (x0, y0, width, height), None if not needed
+        :returns: the prediction as a bool or float depending on the
+                  return_boolean parameter
+        """
         if len(img_to_str('tesseract', image_path)) > 1:
             prediction = 1.0
         else:
--- imgfilter/utils/pytesseract.py
+++ imgfilter/utils/pytesseract.py
@@ -8,8 +8,8 @@ def run_tesseract(tesseract_path, infile, outfile):
     proc = subprocess.Popen(command, stderr=subprocess.PIPE)
     return (proc.wait(), proc.stderr.read())
 
-
 def img_to_str(tesseract_path, image):
+    """reads text in a image with the tesseract-ocr."""
     with tempfile.NamedTemporaryFile(prefix="tess_") as outfile:
         try:
             status, err_str = run_tesseract(tesseract_path, image, outfile.name)
@@ -17,13 +17,10 @@ def img_to_str(tesseract_path, image):
                 errors = get_errors(error_string)
                 raise TesseractErr(status, errors)
             with open(outfile.name + ".txt") as out:
-                try:
-                    return out.read().strip()
-                finally:
-                    out.close()
+                return out.read().strip()
         finally:
-			cleanup(outfile.name + ".txt")
-		   
+            cleanup(outfile.name + ".txt")
+           
 def cleanup(filename):
     try:
         os.remove(filename)
